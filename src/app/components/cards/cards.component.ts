916a55a5ef905020643decc0422315ced4309fbe
georgee99 <g.elzakhem99@gmail.com>
2021-10-29 16:49:11 +1100

    Filters + homepage + package.json

--- src/app/components/cards/cards.component.ts
+++ src/app/components/cards/cards.component.ts
@@ -1,8 +1,5 @@
-import { Component, OnInit, Input, EventEmitter, Output } from '@angular/core';
-import { MoviesInt } from 'src/app/MoviesInt';
+import { Component, OnInit } from '@angular/core';
 import { MovieService } from 'src/app/services/movie.service';
-import { Pipe, PipeTransform } from '@angular/core';
-import { FilterPipe } from 'src/app/filter.pipe';
 
 @Component({
   selector: 'app-cards',
@@ -10,16 +7,11 @@ import { FilterPipe } from 'src/app/filter.pipe';
   styleUrls: ['./cards.component.css']
 })
 export class CardsComponent implements OnInit {
-  myArr: any = [] //Genres
   myMovieArr: any = []
-  @Input() dell: any;
   removedMovies: any[] = [];
-  @Input() checkOptionsOne:any = [
-    { label: 'Happy', value: 'Happy', checked:false},
-    { label: 'Sad', value: 'Sad' , checked:false},
-    { label: 'Meh', value: 'Meh' , checked:false}
-  ];
-  enableFilter = false;
+  enableFilter:Object = false;
+  enableFilterSad:Object = false;
+  enableFilterMeh:Object = false;
   
   constructor(private movie: MovieService) {}
 
@@ -28,38 +20,16 @@ export class CardsComponent implements OnInit {
       console.warn(movie)
       this.myMovieArr = movie;
       for(let i =0; i<this.myMovieArr.length; i++){
-        if(localStorage.getItem(this.myMovieArr[i]["id"] + 'toDelete') !== null){
-          // this.myMovieArr = this.myMovieArr.filter((item: any) => item !==this.myMovieArr[i])
+        let a:any = localStorage.getItem(this.myMovieArr[i]["id"] + "toDelete")
+        if(a !== null){
+          console.log(a)
+          this.myMovieArr = this.myMovieArr.filter((item: any) => item !==this.myMovieArr[i])
+          this.removedMovies.push(this.myMovieArr[i])
+          localStorage.setItem('removedMoviesList', JSON.stringify(this.removedMovies))
         }
       }
     })
-  } // End of ngOnInit
-
-
-  log(value:any) {
-    console.log(value[1]["label"]);
-    // let movieObj:any = localStorage.getItem('580489emotion')
-    // movieObj = JSON.parse(movieObj)
-    // if(movieObj.emotion ==='happy'){
-    //   console.log("happy")
-    // }
-    for(let i =0; i<this.myMovieArr.length; i++){
-      if(localStorage.getItem(this.myMovieArr[i]["id"] + 'emotion') !== null){
-        console.log(this.myMovieArr[i]["title"] + " has local storage in it")
-      }
-    }
-    for(let i =0; i<3; i++){
-      if(value[i]["label"] === "Sad"){
-        console.log('works')
-      }
-    }
-  }
-
-  getMovieGenres(): void {
-    this.movie.getMovieGenres().subscribe(movie => {
-      this.myArr = movie;
-    })
-  }
+  } 
 
   getSciMovies(): void {
     this.movie.getMovies().subscribe(movie => {
@@ -67,17 +37,4 @@ export class CardsComponent implements OnInit {
       this.myMovieArr = movie;
     })
   }
-
-  // Dont need removeMovie method anymore
-
-  removeMovie(answer: any): void {
-    answer = prompt("Which movie you wanna delete")
-    let removedMovie = this.myMovieArr[answer]["title"]
-    this.removedMovies.push(removedMovie)
-    console.log(this.removedMovies)
-    this.myMovieArr = this.myMovieArr.filter((item: any) => item!==this.myMovieArr[answer])
-    console.log(answer)
-    localStorage.setItem('removedmovie', JSON.stringify(this.removedMovies))
-  }
-
 }
--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -1,17 +1,48 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'filter'
+  name: 'filter',
+  pure: false
 })
 export class FilterPipe implements PipeTransform {
 
-  transform(products:any[], filter: Object): any[] {
-    // return products.filter((product:any) => {
-    //   return product.id < 58041;
-    // });
-    // Change this up to filter for emotions
+  transform(products:any[], filter: Object, filterSad: Object, filterMeh: Object): any[] {
+
     if(!products || filter){
-      return products.filter((item:any) => item.id > 550988);
+      let certainMovies = [];
+      for(let i =0; i<products.length; i++){
+        let m:any = localStorage.getItem(products[i]["id"] + "emotion")
+        m = JSON.parse(m)
+        if(m !== null && m["emotion"] == "happy"){
+          certainMovies.push(products[i])
+        }
+      }
+      return certainMovies;
+      // return products.filter((item:any) => item.id > 550988);
+    }
+
+    if(!products || filterSad){
+      let sadMovies = [];
+      for(let i =0; i<products.length; i++){
+        let s:any = localStorage.getItem(products[i]["id"] + "emotion")
+        s = JSON.parse(s)
+        if(s !== null && s["emotion"]=="sad"){
+          sadMovies.push(products[i])
+        }
+      }
+      return sadMovies;
+    }
+
+    if(!products || filterMeh){
+      let mehMovies = [];
+      for(let i =0; i<products.length; i++){
+        let meh:any = localStorage.getItem(products[i]["id"] + "emotion")
+        meh = JSON.parse(meh)
+        if(meh !== null && meh["emotion"]=="meh"){
+          mehMovies.push(products[i])
+        }
+      }
+      return mehMovies;
     }
 
     return products
