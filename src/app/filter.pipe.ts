e6d5c990ac202739970c4dab4a4f6510ec7f2acc
georgee99 <g.elzakhem99@gmail.com>
2022-02-12 23:08:45 +1100

    Auto aligning/formatting

--- src/app/filter.pipe.ts
+++ src/app/filter.pipe.ts
@@ -46,5 +46,4 @@ export class FilterPipe implements PipeTransform {
 
     return products
   }
-
 }
--- src/app/search-filter.pipe.ts
+++ src/app/search-filter.pipe.ts
@@ -4,9 +4,7 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'searchFilter'
 })
 export class SearchFilterPipe implements PipeTransform {
-
   transform(movies: any[], filterText: string): any {
     return movies ? movies.filter((item) => item.title.search(new RegExp(filterText, 'i')) > -1): movies;;
   }
-
 }
